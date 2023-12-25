5e8322f76700454a2375b450d41c06f65b34907c
Pablo Busatto <55034604+PabloBF@users.noreply.github.com>
2023-12-25 11:24:26 -0300

    Create compose.sh

---
+++ avaliacoes/avaliacao-08/compose.sh
@@ -0,0 +1,31 @@
+git clone https://github.com/docker/getting-started-app.git
+cd getting-started-app
+cat > compose.yml << EOL
+services:
+  app:
+    image: node:18-alpine
+	command: sh -c "yarn install && yarn run dev"
+	ports:
+	  - 127.0.0.1:3000:3000
+	  working_dir: /app
+	  volumes:
+	    - ./:/app
+      environment:
+	    MYSQ_HOST: mysql
+		MYSQL_USER: root
+		MYSQL_PASSWORD: secret
+		MYSQL_DB: todos
+	
+	mysql:
+	  image: mysql:8.0
+	  volumes:
+	    - todo-mysql-data:/var/lib/mysql
+	  environment:
+	    MYSQL_ROOT_PASSWORD: secret
+		MYSQL_DATABASE: todos
+
+volumes:
+  todo-mysql-data:
+EOL
+sudo docker compose up --detach
+sudo docker compose ps
